966623f93706908aa598c580bd9d656f0a5731cc
alester77 <113203488+alester77@users.noreply.github.com>
2023-09-03 14:29:00 -0600

    working on index and shapes js

---
+++ index.js
@@ -0,0 +1,73 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const { Triangle, Square, Circle } = require('./lib/shapes');
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'What text would you like to display in your logo? Enter up to 3 characters',
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'What color is the shape?',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'What shape would you like to use?',
+      choices: ['Triangle', 'Square', 'Circle'],
+    },
+    {
+      type: 'input',
+      name: 'backgroundColor',
+      message: 'What color is the background? You can choose a color keyword or hexadecimal value.',
+    }
+  ])
+  .then((answers) => {
+    if (answers.text.length > 3) {
+      console.log('Please enter up to 3 characters.');
+      return promptUser();
+    } else {
+      writeToFile('logo.svg', answers);
+    }
+  
+});
+};
+
+const generateSVG = (answers) => {
+  let shape = "";
+  switch (answers.shape) {
+    case 'Triangle':
+      shape = new Triangle();
+      break;
+    case 'Square':
+      shape = new Square();
+      break;
+    case 'Circle':
+      shape = new Circle();
+      break;
+      default:
+        break;
+  }
+
+  shape.setColor(answers.textColor);
+
+  return `
+  <svg width="300" height="300" style="background-color:${answers.backgroundColor}">
+      ${shape.render()}
+      <text x="150" y="150" dominant-baseline="middle" text-anchor="middle" fill="white">${answers.text}</text>
+    </svg>`;
+};
+
+const writeToFile = (fileName, data) => {
+  const svgString = generateSVG(data);
+  fs.writeFileSync(fileName, svgString);
+};
+
+
+
+promptUser();

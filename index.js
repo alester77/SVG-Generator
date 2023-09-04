c37c27810d79cafdbccf945e28b6f92b568c6da5
alester77 <113203488+alester77@users.noreply.github.com>
2023-09-03 20:47:48 -0600

    fixed shape centering, had to change tests to work correctly

--- index.js
+++ index.js
@@ -1,66 +1,79 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const { Triangle, Square, Circle } = require('./lib/shapes');
+const { Triangle, Square, Circle } = require("./lib/shapes");
 
 const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'text',
-      message: 'What text would you like to display in your logo? Enter up to 3 characters',
-    },
-    {
-      type: 'input',
-      name: 'textColor',
-      message: 'What color is the shape? Keyword or hexidecimal value with an #.',
-    },
-    {
-      type: 'list',
-      name: 'shape',
-      message: 'What shape would you like to use?',
-      choices: ['Triangle', 'Square', 'Circle'],
-    },
-    {
-      type: 'input',
-      name: 'backgroundColor',
-      message: 'What color is the background? You can choose a color keyword or hexadecimal value with a #.',
-    }
-  ])
-  .then((answers) => {
-    if (answers.text.length > 3) {
-      console.log('Please enter up to 3 characters.');
-      return promptUser();
-    } else {
-      writeToFile('logo.svg', answers);
-    }
-  
-});
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "text",
+        message:
+          "What text would you like to display in your logo? Enter up to 3 characters",
+      },
+      {
+        type: "input",
+        name: "textColor",
+        message:
+          "What color is the shape? Keyword or hexidecimal value with an #.",
+      },
+      {
+        type: "list",
+        name: "shape",
+        message: "What shape would you like to use?",
+        choices: ["Triangle", "Square", "Circle"],
+      },
+      {
+        type: "input",
+        name: "backgroundColor",
+        message:
+          "What color is the background? You can choose a color keyword or hexadecimal value with a #.",
+      },
+    ])
+    .then((answers) => {
+      if (answers.text.length > 3) {
+        console.log("Please enter up to 3 characters.");
+        return promptUser();
+      } else {
+        writeToFile("logo.svg", answers);
+      }
+    });
 };
 
 const generateSVG = (answers) => {
   let shape = "";
   switch (answers.shape) {
-    case 'Triangle':
+    case "Triangle":
       shape = new Triangle();
       break;
-    case 'Square':
+    case "Square":
       shape = new Square();
       break;
-    case 'Circle':
+    case "Circle":
       shape = new Circle();
       break;
-      default:
-        break;
+    default:
+      break;
   }
 
   shape.setColor(answers.textColor);
 
+  const centerX = 150;
+  const centerY = 100;
+
   return `
-  <svg width="300" height="300" style="background-color:${answers.backgroundColor}">
+  <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200" viewBox="0 0 300 200">
+    <rect x="0" y="0" width="300" height="200" fill="${
+      answers.backgroundColor
+    }" />
+    <g transform="translate(${centerX}, ${centerY})">
       ${shape.render()}
-      <text x="150" y="150" dominant-baseline="middle" text-anchor="middle" fill="white">${answers.text}</text>
-    </svg>`;
+      <text x="0" y="0" dominant-baseline="middle" text-anchor="middle" fill="white">${
+        answers.text
+      }</text>
+    </g>
+  </svg>`;
 };
 
 const writeToFile = (fileName, data) => {
@@ -72,7 +85,5 @@ const writeToFile = (fileName, data) => {
     console.error("An error occurred while writing the SVG file.", error);
   }
 };
-
-
 
 promptUser();
